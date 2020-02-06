c6a963c9110e4f2563f4f5e0b4c6fde5208aee2a
muhammadafdhal <muhammadafdhalff2014@gmail.com>
2020-02-06 23:58:12 +0700

    validasi perhitungan

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
 class LoginController extends Controller
 {
     /*
@@ -25,7 +29,47 @@ class LoginController extends Controller
      *
      * @var string
      */
+
     protected $redirectTo = '/home';
+
+    public function login(Request $request)
+    {
+        $result = $_POST["result"];
+        $firstnumber = $_POST["firstnumber"];
+        $secondnumber = $_POST["secondnumber"];
+
+        $checktotal = $firstnumber + $secondnumber;
+
+        if ($result == $checktotal) {
+
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    } else {
+        # code...
+        return redirect('/login');
+    }
+    }
+    
 
     /**
      * Create a new controller instance.
