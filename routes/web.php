a02e661aeab50e372b3cdda8ede8a1a835301b8e
muhammadafdhal <muhammadafdhalff2014@gmail.com>
2020-02-11 21:39:03 +0700

    route nilai, absen siswa, galeri & informasi

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,26 @@ Route::resource('/siswa', 'SiswaController');
 Route::resource('/tamu', 'BukutamuController');
 Route::resource('/materi', 'MateriController');
 Route::resource('/mata-pelajaran', 'MapelController');
+Route::resource('/kelas', 'KelasController');
+Route::resource('/jadwal','JadwalPelajaranController');
+Route::resource('/absen','AbsensiswaController');
+Route::resource('/informasi','InfosekolahController');
+Route::resource('/nilai','NilaiController');
+
+//route galeri
+Route::resource('/galeri','GaleriController');
+Route::get('/storage/gl/{namafile}', function ($namafile) {
+    $path=storage_path('app/galeri/'.$namafile);
+
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file=File::get($path);
+    $type=File::mimeType($path);
+    $response=Response::make($file, 200);
+    $response->header("Content-type", $type);
+
+    return $response;
+});
